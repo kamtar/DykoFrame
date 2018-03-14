6c5913b717516b424484cd66d5ec83497cd9413d
DESKTOP-G5R9HKB\tomul <kammtar@gmail.com>
2018-03-14 17:34:08 +0100

    collision manager

---
+++ DykoFrame/CollisionUtils.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DykoFrame
+{
+    public delegate void CollisionTrigger(GameObject o);
+
+    public enum CollisionMatch
+    {
+        Tag,
+        Name,
+        Instantion
+    };
+
+    public struct CollisionEntry
+    {
+        public CollisionMatch type;
+        public bool inclusive;
+        public string name;
+        public GameObject obj;
+        public CollisionTrigger callback;
+    }
+}
