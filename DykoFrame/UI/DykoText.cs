15e2a4907a2e4788544b10317513ecb3bd449974
KAMTAR-PC\Kamtar <kammtar@gmail.com>
2018-03-18 18:15:58 +0100

    change of structure, added DykoText

---
+++ DykoFrame/UI/DykoText.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DykoFrame
+{
+    public class DykoText : MonoBehaviour
+    {
+        private string parse_text;
+        private UnityEngine.UI.Text uiText;
+
+        public UnityEngine.UI.Text UIText
+        {
+            get { return uiText; }
+            set { uiText = value; }
+        }
+
+        void Start()
+        {
+            uiText = GetComponent<UnityEngine.UI.Text>();
+            parse_text = uiText.text;
+        }
+
+        public string this[string key]
+        {
+            set
+            {
+                uiText.text = parse_text.Replace( "%" + key, value);
+            }
+        }
+    }
+}
